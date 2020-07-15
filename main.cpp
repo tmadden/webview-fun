15dd819eea2f39b2355211c425e0fd946de8c360
Thomas Madden <tmadden@gmail.com>
2020-07-15 18:59:51 -0400

    Integrate Hunter & GLFW.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
-// main.c
 #include "webview.h"
+
+#define GLFW_EXPOSE_NATIVE_WIN32
+
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+static void error_callback(int error, const char *description)
+{
+    fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+static webview::webview *the_webview;
+
+static void window_size_callback(GLFWwindow *window, int width, int height)
+{
+    the_webview->set_size(width, height, WEBVIEW_HINT_FIXED);
+}
+
 #ifdef WIN32
 int WINAPI WinMain(HINSTANCE hInt, HINSTANCE hPrevInst, LPSTR lpCmdLine,
                    int nCmdShow)
@@ -10,10 +36,67 @@ int main()
 #endif
     SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 
-    webview::webview w(false, nullptr);
-    w.set_title("Minimal example");
-    w.set_size(1200, 800, WEBVIEW_HINT_MIN);
+    GLFWwindow *window;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    glfwSetKeyCallback(window, key_callback);
+
+    glfwSetWindowSizeCallback(window, window_size_callback);
+
+    HWND hwnd = glfwGetWin32Window(window);
+    webview::webview w(true, &hwnd);
+    the_webview = &w;
     w.navigate("http://127.0.0.1:8000/");
-    w.run();
-    return 0;
+    // w.run();
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        glMatrixMode(GL_MODELVIEW);
+
+        glLoadIdentity();
+        glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
